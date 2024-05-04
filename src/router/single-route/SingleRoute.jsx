e7e14a4dc1fdc8de0617a9566781144a9ca9ae07
QosimovRahmonjonn <qosimovrahmonjon2023@gmail.com>
2024-05-04 11:20:07 +0500

    vercel.json added

--- src/router/single-route/SingleRoute.jsx
+++ src/router/single-route/SingleRoute.jsx
@@ -29,8 +29,8 @@ const SingleRoute = () => {
   console.log(data);
   return (
     <motion.section
-      initial={{ width: 0 }}
-      animate={{ width: "100%" }}
+      initial={{ transform: "scale(.9)" }}
+      animate={{ transform: "scale(1)" }}
       exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}
       className="single__route"
     >
