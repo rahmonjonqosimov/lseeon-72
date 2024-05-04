ee628acc517f2fee1af9f7313ca6abce39c5323c
QosimovRahmonjonn <qosimovrahmonjon2023@gmail.com>
2024-05-04 15:41:53 +0500

    loading animeted add

--- src/router/not-found/NotFound.jsx
+++ src/router/not-found/NotFound.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import img from "../../assets/images/not Found.jpg";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const NotFound = () => {
   let navigate = useNavigate();
   return (
-    <div style={{ textAlign: "center" }} className="container">
+    <motion.div
+      initial={{ width: 0 }}
+      animate={{ width: "100%" }}
+      exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}
+      style={{ textAlign: "center" }}
+      className="container"
+    >
       <img style={{ width: "100%" }} src={img} alt="" />
       <button onClick={() => navigate(-1)} className="back">
         Goo Back
       </button>
-    </div>
+    </motion.div>
   );
 };
 
